71afb4cec23389ebd417ce9db58165b6e5b42025
Kevin Cantu <me@kevincantu.org>
2012-01-11 00:29:04 -0800

    fixing the run script

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-LEVEL=0
+LEVEL=2
 
    rustc --opt-level $LEVEL -L ./djb --lib ./djb/djb.rs \
-&& rustc --opt-level $LEVEL -L . --lib str2.rs \
-&& rustc --opt-level $LEVEL -L . --lib vec2.rs \
+&& rustc --opt-level $LEVEL -L ../rust-tools --lib ../rust-tools/str2.rs \
+&& rustc --opt-level $LEVEL -L ../rust-tools --lib ../rust-tools/vec2.rs \
 && rustc --opt-level $LEVEL -L . --lib murmur.rs \
 && rustc --opt-level $LEVEL -L . test.rs -o test \
-&& rustc --opt-level $LEVEL -L . -L ./djb bench.rs -o bench \
+&& rustc --opt-level $LEVEL -L . -L ./djb -L ../rust-tools bench.rs -o bench \
 && ./test \
 && ./bench
 
