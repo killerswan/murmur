63a36a4159b116b1da7a5cc7e45e6149ba7f4ab2
Kevin Cantu <me@kevincantu.org>
2012-01-11 00:34:13 -0800

    more descriptive message :D

--- djb/bench.rs
+++ djb/bench.rs
@@ -7,6 +7,7 @@ use str2;
 
 fn word_of_god () -> [str]
 {
+   std::io::println ("Loading the lolcat bible...");
    let path = "../lolcat/LOLCatBible_2012-01-04.xml";
    let bible = std::io::read_whole_file (path);
    let bible_ = str::unsafe_from_bytes (result::get (bible));
