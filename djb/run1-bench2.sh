4c24bc6ded7e6173c94e869b7af5ef3d51020e61
Kevin Cantu <me@kevincantu.org>
2012-01-13 21:56:31 -0800

    less shell script clutter

--- djb/run1-bench2.sh
+++
@@ -1,9 +0,0 @@
-echo "$(basename "$0")"
-   rustc --opt-level 0 -L . --lib str2.rs \
-&& rustc --opt-level 0 -L . --lib vec2.rs \
-&& rustc --opt-level 0 -L . --lib djb.rs \
-&& rustc --opt-level 0 -L . --lib bench.rs \
-&& rustc --opt-level 1 -L . --lib bench.rs \
-&& rustc --opt-level 0 -L . main.rs -o main \
-&& ./main
-
